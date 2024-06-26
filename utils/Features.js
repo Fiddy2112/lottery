0103f3206eaa07271a85ad2ce3fa7ee099626dac
Fiddy <thnhlong2112@gmail.com>
2024-06-26 16:18:29 +0700

    create randomNumber function

--- utils/Features.js
+++ utils/Features.js
@@ -1,9 +1,8 @@
-export const showAddress = (address) => {
-  return `${address.substring(0, 4)}...${address.substring(
+export const showAddress = (address) =>
+  `${address.substring(0, 4)}...${address.substring(
     address.length - 0,
     address.length - 4
   )}`;
-};
 
 export const numberFormat = (number) =>
   new Intl.NumberFormat().format(Number(number));
